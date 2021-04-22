ab67602c0e3638aa607215bca08c3c73a394d285
Luke Eberhart-Phillips <luke.eberhart@gmail.com>
2021-04-22 15:10:32 +0200

    sample dist figure

--- R/results_sample_summary.R
+++ R/results_sample_summary.R
@@ -5,7 +5,7 @@ source("R/project_plotting.R")
 
 load("data/ceuta_egg_chick_female_data.rds")
 load("data/raw_encounter_histories_females_2006_2020.rds")
-
+load("output/BaSTA_age_estimates_2006-2020.rds")
 
 # extract the first and last observations of each individual
 ceuta_egg_chick_female_data_3_year <-  
@@ -94,16 +94,37 @@ eggdf_age_CI$ring_ordered <-
                                                  decreasing = TRUE)]), 
          ordered = TRUE)
 
-# specify the factor levels according to the rank (for plotting)
-matings_without_egg_obs_plot <- 
-  matings_without_egg_obs %>% 
-  dplyr::filter(ring %in% ceuta_egg_chick_female_data_3_year$ring)
+# # join polyandry information
+# eggdf_mating_sys_all <-
+#   dbReadTable(Ceuta_OPEN, "BirdRef") %>% 
+#   dplyr::select(year, ID, male, female) %>%
+#   filter(female %in% eggdf$ring) %>% 
+#   arrange(female) %>%
+#   group_by(female, year) %>%
+#   summarise(n_mates = n_distinct(male, na.rm = TRUE)) %>% 
+#   mutate(polyandry = ifelse(n_mates > 1, "poly", "mono"),
+#          year = as.integer(year),
+#          n_mates = ifelse(n_mates == 0, 1, n_mates)) %>% 
+#   rename(ring = female)
+# 
+# matings_without_egg_obs <- 
+#   anti_join(eggdf_mating_sys_all, eggdf, 
+#             by = c("ring", "year")) %>% 
+#   left_join(., dplyr::select(eggdf, ring, age_first_cap, est_b), by = "ring") %>% 
+#   mutate(est_age = year - est_b) %>% 
+#   distinct()
 
-matings_without_egg_obs_plot$ring_ordered <- 
-  factor(matings_without_egg_obs_plot$ring, 
-         levels = levels(eggdf_age_CI$ring_ordered),
-         ordered = TRUE)
-
+# # specify the factor levels according to the rank (for plotting)
+# matings_without_egg_obs_plot <- 
+#   matings_without_egg_obs %>% 
+#   dplyr::filter(ring %in% ceuta_egg_chick_female_data_3_year$ring)
+# 
+# matings_without_egg_obs_plot$ring_ordered <- 
+#   factor(matings_without_egg_obs_plot$ring, 
+#          levels = levels(eggdf_age_CI$ring_ordered),
+#          ordered = TRUE)
+# 
+# dplyr::filter(ceuta_egg_chick_female_data_3_year, is.na(volume) & !is.na(polyandry))
 
 # specify point size for number of clutches per year
 point_size <- c(1, 2, 3, 4)
@@ -122,11 +143,11 @@ Imm_plot <-
   geom_point(data = dplyr::filter(eggdf_clutches, age_first_cap == "A"), 
              aes(x = est_age, y = ring_ordered, size = as.factor(n_clutches), 
                  fill = as.factor(polyandry)), shape = 21) +
-  geom_point(data = dplyr::filter(matings_without_egg_obs_plot, age_first_cap == "A"),
-             aes(x = est_age, y = ring_ordered), size = 2, shape = 4) +
+  # geom_point(data = dplyr::filter(matings_without_egg_obs_plot, age_first_cap == "A"),
+  #            aes(x = est_age, y = ring_ordered), size = 2, shape = 4) +
   scale_x_continuous(limits = c(0, 24),
                      breaks = c(0:24)) +
-  ylab("Females first captured as adults") +
+  ylab("First captured as adults") +
   xlab("Age ± 95% CI") +
   luke_theme +
   theme(legend.position = c(0.78, 0.8),
@@ -135,7 +156,10 @@ Imm_plot <-
   scale_size_manual(values = point_size) +
   scale_fill_manual(values = point_colors) +
   labs(size = "Number of clutches observed",
-       fill = "Number of mates observed")
+       fill = "Number of mates observed") +
+  annotate(geom = "text", y = 28, x = 16,
+           label = "For visual purposes, only individuals\nwith more than 2 years of\nobservations are shown",
+           color = "black", size = 3, fontface = 'italic', hjust = 0)
 
 # create the recruit plot
 Rec_plot <- 
@@ -147,11 +171,12 @@ Rec_plot <-
   geom_point(data = dplyr::filter(eggdf_clutches, age_first_cap == "J"), 
              aes(x = est_age, y = ring_ordered, size = as.factor(n_clutches), 
                  fill = as.factor(polyandry)), shape = 21) +
-  geom_point(data = dplyr::filter(matings_without_egg_obs_plot, age_first_cap == "J"),
-             aes(x = est_age, y = ring_ordered), size = 2, shape = 4) +
+  
+  # geom_point(data = dplyr::filter(matings_without_egg_obs_plot, age_first_cap == "J"),
+  #            aes(x = est_age, y = ring_ordered), size = 2, shape = 4) +
   scale_x_continuous(limits = c(0, 23),
                      breaks = c(0:23)) +
-  ylab("Locally recruited females") +
+  ylab("Locally recruited") +
   xlab("Age") +
   luke_theme +
   theme(legend.position = "none",
@@ -161,13 +186,15 @@ Rec_plot <-
   scale_size_manual(values = point_size) +
   scale_fill_manual(values = point_colors)
 
-
 # draw the two panels together for Fig 1
 plot_of_sample_population <- 
   ggarrange(Rec_plot, Imm_plot, nrow = 2, align = "v",
+            # heights = c(0.5, 0.5))
             heights = c(0.19, 0.81))
 
+plot_of_sample_population
+
 ggsave(plot = plot_of_sample_population,
-       filename = "results/figures/revision/Fig_2.jpg",
+       filename = "products/figures/sample_distribution_plot.jpg",
        width = 7,
        height = 10, units = "in")
