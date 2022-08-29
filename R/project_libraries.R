fc79cd9a232fb880e856fb2a3068bd07b66a3d90
leberhartphillips <luke.eberhart@gmail.com>
2022-08-29 11:08:29 +0200

    optimized library call script

--- R/project_libraries.R
+++ R/project_libraries.R
@@ -1,48 +1,27 @@
-# libraries
-library(arm)
-library(aspace)
-library(BaSTA)
-library(bayestestR)
-library(broom.mixed)
-library(coefplot2)
-library(cowplot)
-library(data.table)
-library(doParallel)
-library(effects)
-library(extrafont)
-library(flexmix)
-library(fGarch)
-library(ggmap)
-library(ggpubr)
-library(ggridges)
-library(ggthemes)
-library(grid)
-library(gt)
-library(kableExtra)
-library(leaflet)
-library(lme4)
-library(magick)
-library(mapview)
-library(mgcv)
-library(moveVis)
-library(multcomp)
-library(multipanelfigure)
-library(parallel)
-library(parameters)
-library(partR2)
-library(patchwork)
-library(RColorBrewer)
-library(rptR)
-library(RSQLite)
-library(scales)
-library(snowfall)
-library(sp)
-library(standardize)
-library(tidybayes)
-library(tidyverse)
-library(rmdformats)
+## install dependent packages
+
+# a vector of all the packages needed in the project's scripts
+packages_required_in_project <- 
+  c("arm", "aspace","BaSTA","bayestestR","broom.mixed", "coefplot2", "cowplot",
+    "data.table", "doParallel", "effects", "extrafont", "flexmix", "fGarch", 
+    "ggmap", "ggpubr", "ggridges", "ggthemes", "grid", "gt", "kableExtra", 
+    "leaflet", "lme4", "magick", "mapview", "mgcv", "moveVis", "multcomp", 
+    "multipanelfigure", "parallel", "parameters", "partR2", "patchwork", 
+    "RColorBrewer", "rptR", "RSQLite", "scales", "snowfall", "sp", 
+    "standardize", "tidybayes", "tidyverse", "rmdformats")
+
+# of the required packages, check if some need to be installed
+new.packages <- 
+  packages_required_in_project[!(packages_required_in_project %in% 
+                                   installed.packages()[,"Package"])]
+
+# install all packages that are not locally available
+if(length(new.packages)) install.packages(new.packages)
+
+# load all the packages into the current R session
+lapply(packages_required_in_project, require, character.only = TRUE)
 
 # connect to CeutaCLOSED
 CeutaCLOSED <- 
   dbConnect(SQLite(), 
-            dbname = "/Users/Luke/Documents/Academic_Projects/Postdoc_Seewiesen/Ceuta_Open/Ceuta_CLOSED/data/Ceuta_CLOSED_version_releases/Ceuta_CLOSED_v2-0-0.sqlite")
+            dbname = "/Users/luketheduke2/Documents/Academic/Mexico/Ceuta_CLOSED/data/Ceuta_CLOSED_version_releases/Ceuta_CLOSED_v2-0-0.sqlite")
